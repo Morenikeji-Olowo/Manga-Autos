44e5a381950a774555da0512287a63a688202997
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-03-27 16:35:05 +0100

    fixed issue

--- index.js
+++ index.js
@@ -34,11 +34,16 @@ app.use((req, res, next)=>{
 
 
 //middlewares
-const corseOptions = {
-    origin: ["http://localhost:5174", "https://smota-livid.vercel.app"],
-    credentials: true,
+
+const corsOptions = {
+  origin: ["http://localhost:5174", "https://smota-livid.vercel.app"],
+  credentials: true,
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 };
-app.use(cors(corseOptions));
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // handle preflight requests
 app.use(json({limit: "100mb"}));
 app.use(urlencoded({limit: "100mb", extended: true}));
 app.use(session({

d65f71e8087f8d8c79b84e430d46995970986af6
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-03-29 22:25:33 +0100

    fixing cors issues

--- models/User.js
+++ models/User.js
@@ -72,14 +72,14 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async (next)=>{
-  if(!this.isModified("password") || !this.password) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password") || !this.password) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
-})
+});
 
-userSchema.methods.comparePassword = async (plainPassword) =>{
-  return bcrypt.compare(plainPassword, this.password)
+userSchema.methods.comparePassword = async function (plainPassword) {
+  return bcrypt.compare(plainPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);

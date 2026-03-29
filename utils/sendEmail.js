7df185603038380d0b20d574364aa34ccc234852
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-03-29 23:41:19 +0100

    fixing nodemailer issues

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,25 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-export const sendEmail = async (to, subject, text) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, 
-      family: 4,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text,
-    });
-  } catch (err) {
-    console.log(`nodemailer error: ${err}`);
-  }
-};
+export const sendEmail = async (to, subject, text) =>{
+    try{
+        const {error} = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to,
+            subject,
+            html: `<p>${text}</p>`,
+        })
+
+        if(error){
+            console.error(error.message);
+            throw new Error(error.message)            
+        }
+
+        console.log(`email sent to: ${to}`)
+    }
+    catch(err){
+        console.error(err);
+        throw err
+    }
+}

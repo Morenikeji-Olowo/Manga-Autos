214e11af2253c900417da925e64809929f95f290
Morenikeji-Olowo <morenikejiolowo101@gmail.com>
2026-05-10 19:18:34 +0100

    fixed bravo issues

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,17 +1,17 @@
-import { TransactionalEmailsApi, SendSmtpEmail, ApiClient } from '@getbrevo/brevo';
+import { Brevo } from '@getbrevo/brevo';
 
-const apiInstance = new TransactionalEmailsApi();
-apiInstance.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+const client = new Brevo({
+    apiKey: process.env.BREVO_API_KEY
+});
 
 export const sendEmail = async (to, subject, text) => {
     try {
-        const sendSmtpEmail = new SendSmtpEmail();
-        sendSmtpEmail.to = [{ email: to }];
-        sendSmtpEmail.sender = { email: 'morenikejiolowo101@gmail.com', name: 'Smota' };
-        sendSmtpEmail.subject = subject;
-        sendSmtpEmail.htmlContent = `<p>${text}</p>`;
-
-        await apiInstance.sendTransacEmail(sendSmtpEmail);
+        await client.sendTransactionalEmail({
+            to: [{ email: to }],
+            sender: { email: 'morenikejiolowo101@gmail.com', name: 'Smota' },
+            subject,
+            htmlContent: `<p>${text}</p>`,
+        });
         console.log(`email sent to: ${to}`);
     }
     catch (err) {
